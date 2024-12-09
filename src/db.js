cf03cfde9a099dd64f0c86e76029ff6647f3e8d9
ashishh06 <ashishhariom14@gmail.com>
2024-12-10 00:57:01 +0530

    main

--- src/db.js
+++ src/db.js
@@ -1,21 +1,23 @@
-import {DatabaseSync} from 'node:sqlite' 
-const db=new DatabaseSync(':memory:')
+import Database from 'better-sqlite3';
 
-db.exec(`
-    CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE,
-    password TEXT
-    )
-    `)
-db.exec(`
-    CREATE TABLE todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    task TEXT,
-    completed BOOLEAN DEFAULT 0,
-    FOREIGN KEY(user_id) REFERENCES users(id)
-    )
-    `)
+const db = new Database(':memory:');
+
+db.prepare(`
+  CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE,
+  password TEXT
+  )
+`).run();
+
+db.prepare(`
+  CREATE TABLE todos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  task TEXT,
+  completed BOOLEAN DEFAULT 0,
+  FOREIGN KEY(user_id) REFERENCES users(id)
+  )
+`).run();
 
 export default db;
